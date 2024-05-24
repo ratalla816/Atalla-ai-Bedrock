c749992fd2e58d495d1d2c65a89af97f5b1fa7de
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2024-05-24 11:08:46 -0500

    auth complete

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
-import { Icon } from '@/components/icon';
-import { SignUp } from '@clerk/nextjs';
-import Link from 'next/link';
+import { Icon } from "@/components/icon";
+import { SignUp } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Page() {
   return (
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ['/sign-in', '/sign-up'],
+  publicRoutes: ["/sign-in", "/sign-up"],
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
